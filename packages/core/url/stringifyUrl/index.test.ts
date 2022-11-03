37bfd30e9cfa9f51aa6555613eea9b10434e2a87
yanquanfahei <1449826851@qq.com>
2022-11-03 22:56:39 +0800

    test: test

--- packages/core/url/stringifyUrl/index.test.ts
+++ packages/core/url/stringifyUrl/index.test.ts
@@ -1,12 +1,40 @@
 import { expect, test } from 'vitest'
-import { stringifyUrl, removeHash, extract } from '.'
+import {
+  stringifyUrl,
+  removeHash,
+  extract,
+  splitFirst,
+  decode,
+  formatter
+} from '.'
+
+test('splitFirst', () => {
+  expect(splitFirst('', '=')).toMatchInlineSnapshot([''])
+  expect(splitFirst('foo=2', '=')).toMatchInlineSnapshot(['foo', '2'])
+  expect(splitFirst('bar-2', '=')).toMatchInlineSnapshot(['bar-2'])
+})
 
 test('removeHash', () => {
   expect(removeHash('https://foo/#/bar?foo=1')).toEqual('https://foo/')
-
   expect(removeHash('https://foo/bar?foo=1')).toEqual('https://foo/bar?foo=1')
 })
 
 test('extract', () => {
   expect(extract('https://foo?foo=1/#/bar')).toEqual('foo=1/')
+  expect(extract('https://foo/#/bar?foo=1')).toEqual('')
+  expect(extract('https://foo/bar?foo=1&bar=2')).toEqual('foo=1&bar=2')
+})
+
+test('decode', () => {
+  expect(decode('%E5%BC%A0%E4%B8%89')).toEqual('张三')
+})
+
+test('formatter', () => {
+  const ret = {}
+  formatter('foo', '22', ret)
+  expect(ret).toMatchInlineSnapshot({ foo: '22' })
+  formatter('bar', '33', ret)
+  expect(ret).toMatchInlineSnapshot({ foo: '22', bar: '33' })
+  formatter('bar', '44', ret)
+  expect(ret).toMatchInlineSnapshot({ foo: '22', bar: ['33', '44'] })
 })
