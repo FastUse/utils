5647a54e2092040f11a28a14da15ff3d8a66f295
yanquanfahei <1449826851@qq.com>
2022-11-13 20:28:55 +0800

    test: scroll

--- packages/core/browser/scroll/index.test.ts
+++ packages/core/browser/scroll/index.test.ts
@@ -1,13 +1,20 @@
 import { test, expect } from 'vitest'
 import { JSDOM } from 'jsdom'
-import { setScrollTop } from '.'
 import { ScrollElement } from './types'
+import { setScrollTop, getScrollTop } from '.'
+
+test('getScrollTop', () => {
+  const { window } = new JSDOM()
+
+  expect(
+    getScrollTop(window as unknown as ScrollElement)
+  ).toMatchInlineSnapshot('0')
+})
 
 test('setScrollTop', () => {
-  const { window } = new JSDOM()
-  const { document } = (new JSDOM()).window;
-  
-  setScrollTop(window as unknown as ScrollElement, 10)
-  
-  expect(window.scrollY).toMatchInlineSnapshot('0')
+  const { document } = new JSDOM().window
+
+  setScrollTop(document.documentElement as unknown as ScrollElement, 20)
+
+  expect(document.documentElement.scrollTop).toMatchInlineSnapshot('20')
 })
