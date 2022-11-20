c6bfa58b84c54b4503afbd97e809c8a15bc6bdc4
yanquanfahei <1449826851@qq.com>
2022-11-20 15:23:47 +0800

    chore: base path

--- packages/.vitepress/config.ts
+++ packages/.vitepress/config.ts
@@ -22,6 +22,7 @@ const UrlSideItems: DefaultTheme.SidebarItem[] = [
 export default defineConfig({
   title: 'Utils',
   description: '日常开发遇到的公共方法记录',
+  base: '/utils/',
   themeConfig: {
     logo: 'logo.svg',
     sidebar: {
