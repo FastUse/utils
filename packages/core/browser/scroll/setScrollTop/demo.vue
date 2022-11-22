6595ecfe8c2ff2bc4477ad80906976b153230470
yanquanfahei <1449826851@qq.com>
2022-11-22 20:46:38 +0800

    docs: setScrollTop demo

---
+++ packages/core/browser/scroll/setScrollTop/demo.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { setScrollTop } from '.'
+
+const scrollRef = ref()
+const inputVal = ref(0)
+
+function setTop() {
+  setScrollTop(scrollRef.value, inputVal.value || 0)
+}
+</script>
+
+<template>
+  <div>
+    <section class="mb5">
+      <input v-model="inputVal" type="number" />
+      <button @click="setTop">设置</button>
+    </section>
+    <section ref="scrollRef" class="h100 overflow-y-auto mb5">
+      <div
+        v-for="i in 8"
+        :key="i"
+        class="border h20 mb3 b-rd flex items-center justify-center"
+      >
+        {{ i }}
+      </div>
+    </section>
+  </div>
+</template>
