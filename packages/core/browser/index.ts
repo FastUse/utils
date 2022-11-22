a2fc3035091a4d75fb1999e25dcd0c8b2041e36b
yanquanfahei <1449826851@qq.com>
2022-11-22 22:00:18 +0800

    feat: export member

--- packages/core/browser/index.ts
+++ packages/core/browser/index.ts
@@ -1,1 +1,4 @@
-export * from './scroll'
+export { getScrollTop } from './scroll/getScrollTop'
+export { scrollLeftTo } from './scroll/scrollLeftTo'
+export { scrollTopTo } from './scroll/scrollTopTo'
+export { setScrollTop } from './scroll/setScrollTop'
