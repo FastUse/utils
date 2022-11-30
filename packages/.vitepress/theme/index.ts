b4867309f95784f29dd1b5b2881f4a25792df8ce
yanquanfahei <1449826851@qq.com>
2022-11-30 14:58:22 +0800

    feat: add vconsole

--- packages/.vitepress/theme/index.ts
+++ packages/.vitepress/theme/index.ts
@@ -3,6 +3,11 @@ import DefaultTheme from 'vitepress/theme'
 import './styles/demo.less'
 import './styles/vars.less'
 import 'uno.css'
+import VConsole from 'vconsole'
+
+if(import.meta.env.MODE === "development") {
+  new VConsole()
+}
 
 export default {
   ...DefaultTheme,
