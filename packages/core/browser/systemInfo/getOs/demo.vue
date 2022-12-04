4196d51fc96697f0ebd3a0c3a5007213575be5fa
yanquanfahei <1449826851@qq.com>
2022-12-04 19:45:54 +0800

    docs: getOs demo

---
+++ packages/core/browser/systemInfo/getOs/demo.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { getOs } from '.'
+
+const os = getOs()
+</script>
+
+<template>
+  <div>
+    系统名：{{ os.osName }}
+    <br />
+    系统版本：{{ os.osVersion }}
+    <br />
+    系统语言：{{ os.osLanguage }}
+    <br />
+    系统主题：{{ os.osTheme }}
+  </div>
+</template>
