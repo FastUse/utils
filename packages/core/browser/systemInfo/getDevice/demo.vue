52b5bab9352dab00abec9f7fc927672bccdf2bf1
yanquanfahei <1449826851@qq.com>
2022-12-05 21:54:27 +0800

    docs: getDevice demo

---
+++ packages/core/browser/systemInfo/getDevice/demo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { getDevice } from '.'
+
+const deviceInfo = getDevice()
+</script>
+
+<template>
+  <div>
+    设备id: {{ deviceInfo.deviceId }}
+    <br />
+    设备类型: {{ deviceInfo.deviceType }}
+    <br />
+    设备型号: {{ deviceInfo.deviceModel }}
+    <br />
+    设备方向: {{ deviceInfo.deviceOrientation }}
+    <br />
+    设备像素比: {{ deviceInfo.devicePixelRatio }}
+  </div>
+</template>
