45053b3ba7749f15a35ac510e42f080e5e06185d
yanquanfahei <1449826851@qq.com>
2022-12-10 15:31:05 +0800

    docs: getBrowser demo

---
+++ packages/core/browser/systemInfo/getBrowser/demo.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { getBrowser } from '.'
+
+function setMeta(name: string, content: string) {
+  document.querySelector(`meta[name=${name}]`)?.setAttribute('content', content)
+}
+
+setMeta('viewport', 'width=device-width,initial-scale=1,viewport-fit=cover')
+
+const result = getBrowser()
+</script>
+<template>
+  <div>
+    <code>
+      <pre>{{ JSON.stringify(result, null, 2) }}</pre>
+    </code>
+  </div>
+</template>
+<style>
+#app {
+  padding-bottom: constant(safe-area-inset-bottom); /* 兼容 iOS < 11.2 */
+  padding-bottom: env(safe-area-inset-bottom); /* 兼容 iOS >= 11.2 */
+}
+</style>
