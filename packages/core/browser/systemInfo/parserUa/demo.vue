4f818ef7a037428f2be227b166f3a75a6199f003
yanquanfahei <1449826851@qq.com>
2022-12-10 14:54:09 +0800

    docs: update demo

--- packages/core/browser/systemInfo/parserUa/demo.vue
+++ packages/core/browser/systemInfo/parserUa/demo.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+import { ua as defaultUa } from '../utils'
 import { parserUa } from '.'
 
-const result = parserUa()
+const ua = ref(defaultUa)
+const result = computed(() => parserUa(ua.value))
 </script>
 <template>
   <div>
+    <div class="mb3 flex items-center">
+      <label class="mr3" for="">user-agent</label>
+      <input v-model="ua" type="text" />
+    </div>
     <code>
       <pre>{{ JSON.stringify(result, null, 2) }}</pre>
     </code>
