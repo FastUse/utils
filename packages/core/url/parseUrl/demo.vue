72a4e23e560df0e8270fd3bb365cde10e04b6229
yanquanfahei <1449826851@qq.com>
2022-12-10 14:38:34 +0800

    docs: update demo

--- packages/core/url/parseUrl/demo.vue
+++ packages/core/url/parseUrl/demo.vue
@@ -2,14 +2,25 @@
 import { computed, ref } from 'vue'
 import { parseUrl } from '.'
 
-const url = ref('https://www.baidu.com?foo=1')
+const isRemoveHash = ref(true)
 
-const result = computed(() => JSON.stringify(parseUrl(url.value)))
+const url = ref('https://www.baidu.com/#/index.html?foo=1')
+
+const result = computed(() =>
+  JSON.stringify(parseUrl(url.value, { removeHash: isRemoveHash.value }))
+)
 </script>
 
 <template>
   <div>
     <section class="mt3">
+      <div class="mb3 flex items-center">
+        <label class="mr3" for="">removeHash</label>
+        <select v-model="isRemoveHash">
+          <option :value="true">true</option>
+          <option :value="false">false</option>
+        </select>
+      </div>
       <div class="flex items-center">
         <label class="mr3" for="">parseUrl</label>
         <input v-model="url" type="text" />
